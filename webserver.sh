5121f0acdc5f9362fed32fba3ec1b274d8d05248
katuri75 <katuri@katuri.kr>
2021-10-26 16:20:39 +0900

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -1,12 +1,12 @@
 firewall-cmd --add-service=http --permanent
 firewall-cmd --reload
 setenforce 0
-mkdir /etc/www
-hostname > /etc/www/index.html
-hostname > /etc/www/hostname.txt
-uptime >> /etc/www/index.html
-cd /etc/www
+mkdir /var/www
+hostname > /var/www/index.html
+hostname > /var/www/hostname.txt
+uptime >> /var/www/index.html
+cd /var/www
 chmod a+x /etc/rc.d/rc.local
-sed -i -e '$i \cat /etc/www/hostname.txt > /etc/www/index.html && uptime >> /etc/www/index.html\n' /etc/rc.d/rc.local
-sed -i -e '$i \cd /etc/www && python -m SimpleHTTPServer 80 &\n' /etc/rc.d/rc.local
+sed -i -e '$i \cat /var/www/hostname.txt > /var/www/index.html && uptime >> /var/www/index.html\n' /etc/rc.d/rc.local
+sed -i -e '$i \cd /var/www && python -m SimpleHTTPServer 80 &\n' /etc/rc.d/rc.local
 python -m SimpleHTTPServer 80 &
